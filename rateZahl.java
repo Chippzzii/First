2b396d08519aa77d475df733545accaef62b9faf
c.hülsmann <115979110+Chippzzii@users.noreply.github.com>
2022-10-30 23:26:54 +0100

    Play again update

--- rateZahl.java
+++ rateZahl.java
@@ -1,57 +1,89 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class rateZahl {
-	 static int searchedInt, rndInt, countTry = 1;
-     static boolean searched = true;
-     static Random rnd = new Random ();
-     static Scanner input = new Scanner(System.in);
+    static int searchedInt, rndInt, countTry = 1;
+    static boolean searched = true;
+    static Random rnd = new Random ();
+    static Scanner input = new Scanner(System.in);
+    static char eingabe = 'y';
 
     public static void main(String[] args) {
-        
 
-    	start();
-    	win();
-    	input.close();
-    	
-    
-    
+
+        game();
+        input.close();
+
+
+
     }	//close Main
     public static void win() {
-	 if(rndInt == searchedInt) {
-		 System.out.println("Der Computer hat die Zahl " + rndInt + " gewählt");
-		 System.out.println("Herzlichen Glückwunsch, du hast die Zahl erraten.");
-		 System.out.println("Du hast " + countTry + " Versuch benötigt um die Zahl zu erraten.");
-	 }
-    
+
+               if(rndInt == searchedInt) {
+
+                   System.out.println("Der Computer hat die Zahl " + rndInt + " gewählt");
+                   sleep();
+                   System.out.println("Herzlichen Glückwunsch, du hast die Zahl erraten.");
+                   sleep();
+                   System.out.println("Du hast " + countTry + " Versuch benötigt um die Zahl zu erraten.");
+                   sleep();
+
+               }
+
+           }
+
+
+    public static void sleep() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(700);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
     public static void start() {
-    	System.out.println("Errate welche Zahl der PC gewählt hat... ");
+        System.out.println("Errate welche Zahl der PC gewählt hat... ");
         rndInt = rnd.nextInt(300) + 1;
         System.out.println(rndInt); //controll ki Input, for actually gameplay, remove this line
 
         System.out.println("Bitte gebe eine Zahl zwischen 1 und 300 an");
-        searchedInt = input.nextInt();        
+        searchedInt = input.nextInt();
 
-    do {
+        do {
 
-        while (searchedInt != rndInt ){
-        	
-        	 if (rndInt > searchedInt) {
-                 System.out.println("Bitte geben Sie eine Zahl ein die Größer ist...");
+            while (searchedInt != rndInt ) {
+
+                if (rndInt > searchedInt) {
+                    sleep();
+                    System.out.println("Bitte geben Sie eine Zahl ein die Größer ist...");
                     countTry++;
+                }  else  {
+                        sleep();
+                        System.out.println("Bitte geben Sie eine Zahl ein die Kleiner ist...");
+                        countTry++;
                 }
+                searchedInt = input.nextInt();
 
-        	 else 
-                {System.out.println("Bitte geben Sie eine Zahl ein die Kleiner ist...");
-                    countTry++;
-                }
-        	 searchedInt = input.nextInt();
-        	 
-        	 
-        	} 
-        
-    	} while (!searched);
-    	
+
+            }
+
+        } while (!searched);
+
     }
-   
+
+    public static void game() {
+        do {
+            while (searchedInt == rndInt) {
+                start();
+                sleep();
+                win();
+
+                System.out.println("---Neue Runde, neues Glück!---");
+                System.out.println("Möchtest du nochmal raten? (Y/N)");
+                eingabe = input.next().charAt(0);
+                sleep();
+
+            }
+        } while (eingabe == 'y');
+    }
+
 }		//close Class
